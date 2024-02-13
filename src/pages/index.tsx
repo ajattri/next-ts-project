642287003bbaddfb5905e5b2272c5e9bd4defb26
ajay kumar <ajay.kumar@galaxyweblinks.in>
2024-02-13 17:09:32 +0530

    next ts project set-up

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <div>
+       Welcome to the page
+      </div>
+    </>
+  );
+}
