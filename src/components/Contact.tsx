1517598a632166142f662356d382fbcf951779e2
ajay kumar <ajay.kumar@galaxyweblinks.in>
2024-02-13 17:48:31 +0530

    added contact page

---
+++ src/components/Contact.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+ const Contact: React.FC<{}> = ({...props }) => {
+
+   return (
+        <div>
+            Contact
+        </div>
+    );
+}
+export default Contact;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
 
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <div>
+      <h2>
        Welcome to the page
-      </div>
+      </h2>
+      <Link href="/contact">
+       link to contacts
+      </Link>
     </>
   );
 }
